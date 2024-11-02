f2f99382e15c790c9ebba038246bb6c4829eb810
Inesh Loka <ineshrloka@gmail.com>
2024-11-02 02:14:25 -0400

    Create table component and format data returned by API

--- Project/client/src/App.jsx
+++ Project/client/src/App.jsx
@@ -1,10 +1,16 @@
 import ChatBox from './ChatBox';
+import Table from './DataTable';
+import { useState } from 'react';
 
 function App() {
-
+  const [tableData, setTableData] = useState(JSON.stringify([
+    { _id: "1", name: "Alice", age: 25, city: "New York" },
+    { _id: "2", name: "Bob", age: 30, city: "Los Angeles" },
+    { _id: "3", name: "Charlie", age: 35, city: "Chicago" }
+  ]));
   const queryAI = async (inputText) => {
     try {
-      const response = fetch('http://localhost:5001/llm/query', {
+      const response = await fetch('http://localhost:5001/llm/query', {
         method: 'POST', 
         headers: {
           'Content-Type': 'application/json',
@@ -13,7 +19,10 @@ function App() {
           prompt: inputText
         })
       });
-      return response;
+      const data = await response.json();
+      console.log(data);
+      setTableData(data);
+      return data;
     } catch (error) {
       console.error('Error Querying Data:', error);
     }
@@ -24,7 +33,8 @@ function App() {
       <h1 className="text-2xl font-bold m-0 p-0 leading-tight">Data Search Tool</h1>
       <div className="flex flex-col border rounded-md w-[700px] h-[600px] bg-white shadow-lg p-4">
         <div className="flex-grow">
-          {/* Content above the input area, e.g., messages */}
+          <Table jsonData={tableData}/>
+
         </div>
         {/* The ChatBox component will be at the bottom */}
         <ChatBox onSubmit={queryAI} />
