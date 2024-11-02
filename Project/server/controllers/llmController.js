a6bb36b4520657c9574b28b9794344dc1488605e
Inesh Loka <ineshrloka@gmail.com>
2024-11-01 23:06:12 -0400

    Fix to return correct data type for select queries

--- Project/server/controllers/llmController.js
+++ Project/server/controllers/llmController.js
@@ -8,21 +8,24 @@ const sample = "8/27/2014 11:29,Female,United States,Corporate,,No,Yes,1-14 days
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 const model = genAI.getGenerativeModel({ 
     model: "gemini-1.5-flash", 
-    systemInstruction: "Given the csv columns " + columns + "; and the sample data " + sample +"; give me a mongodb query for anything I ask for. Do not give me anything besides the query text.",
+    systemInstruction: "Given the csv columns " + columns + "; and the sample data " + sample +"; give me a mongodb query for anything I ask for. Do not give me anything besides the query text. ",
 });
 
+db.collection = db.collection("patients");
 
 export async function query(prompt) {
     const result = await model.generateContent(prompt);
     let query = result.response.text();
     const lines = query.split('\n');
-    db.collection = db.collection("patients");
     
     // Remove the first and last lines
     lines.shift(); 
     lines.pop();  
     query = lines.join('\n'); // Join the remaining lines back into a string
-
+    console.log(query);
+    if (query.includes("find(")) {
+        query += ".toArray()";
+    }
     let queryResult = await eval(query); // query contains a mongodb query. Eval to execute query.
     console.log(queryResult); 
     return queryResult;
